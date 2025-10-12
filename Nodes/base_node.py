5cdc476d42d974f46c08dcbf6e880c6244c7b59e
Andromeda-crypto <145452127+Andromeda-crypto@users.noreply.github.com>
2025-10-12 16:49:04 -0400

    Initial setup

---
+++ Nodes/base_node.py
@@ -0,0 +1,20 @@
+"""
+Creates a Singular Node that behaves like an appliance and has similar characteristics 
+"""
+import random 
+
+
+
+class EnergyNode:
+    def __init__(self):
+        self.energy = 100
+        self.degradation = 0.  #zero deg initially
+        self.efficiency = 1.0  #maximal efficiency to begin with
+
+
+    #  Create a world around the node in which it lives in 
+    
+
+
+
+    
